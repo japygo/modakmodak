fcb1972266032b1778b97ca35307b35572819276
japygo <hyunnjinn@gmail.com>
2025-12-21 14:07:39 +0900

    feat: 기본 Android 프로젝트 초기 설정 추가
    - AndroidManifest 및 기본 리소스 파일 추가
    - Compose 기반의 MainActivity 및 테마 구성 파일 작성
    - Gradle 설정 파일 및 라이브러리 종속성 정의
    - Gitignore 파일을 생성해 불필요한 파일 제외

---
+++ app/src/main/java/com/japygo/modakmodak/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.japygo.modakmodak
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.japygo.modakmodak.ui.theme.ModakModakTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ModakModakTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ModakModakTheme {
+        Greeting("Android")
+    }
+}
