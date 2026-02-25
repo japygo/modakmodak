be0339c3ff055708ca0e6be309711b8bd0589ff9
japygo <hyunnjinn@gmail.com>
2026-02-25 16:05:38 +0900

    chore: 앱 버전 코드 3으로 업데이트
    - 구글 플레이 스토어 배포를 위해 버전 코드 증가
    - 기존 2에서 3으로 변경
    - 앱 기능 및 로직 변경 사항 없음

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.japygo.modakmodak"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
